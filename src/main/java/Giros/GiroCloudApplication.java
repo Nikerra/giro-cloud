36002ee0c10e385415a91e65078ba75e329496e3
nikerra90 <nikerra90@gmail.com>
2023-09-04 14:29:37 +0300

    Выкладка проекта в репозиторий

---
+++ src/main/java/Giros/GiroCloudApplication.java
@@ -0,0 +1,13 @@
+package Giros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiroCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiroCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Giros/GiroCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package Giros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiroCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
